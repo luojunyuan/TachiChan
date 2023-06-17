182119f214a4f193a6f24831c236eb268a3f876d
kimika <kimika@A2E8>
2023-06-18 05:10:13 +0900

    .

--- Preference/MainPage.xaml.cs
+++ Preference/MainPage.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,16 +21,44 @@ using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
-namespace Preference
+namespace Preference;
+
+/// <summary>
+/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+/// </summary>
+public sealed partial class MainPage : Page
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+        InitializeComponent();
+
     }
+
+    private readonly ObservableCollection<ProcessDataModel> _processes = new();
+
+    private void InjectButtonOnClick(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private async void ProcessComboBoxOnDropDownOpened(object sender, object e)
+    {
+        _processes.Add(new ProcessDataModel()
+        {
+            Title = "aaa",
+            Describe = "sdf"
+        });
+        //await Task.Run(RefreshProcesses).ConfigureAwait(false);
+    }
+
+    private void ProcessComboBoxOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        InjectButton.IsEnabled = true;
+    }
+}
+
+public class ProcessDataModel
+{
+    public string Title { get; set; }
+    public string Describe { get; set; }
 }
--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Preference                    uwp           uwp
 ErogeHelper.TextWindow                      net8 native winforms
 ErogeHelper.KeyMapping                      c++ exe
 ErogeHelper.VirtualKeyboard                 net8 native winforms
+
+-windows10.0.19041.0 for convenience tool
+
+publish
+Release -> Package -> Publish Application
+
+only for check icon title resources
+Debug -> Package -> Local computer
+
+debug UWP
