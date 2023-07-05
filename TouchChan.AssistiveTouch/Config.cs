73fd7e0819b37fb3aa89483d0ee9e11fa091c8b3
k1mlka <k1mlka@outlook.com>
2023-07-06 08:07:00 +0900

    .

--- TouchChan.AssistiveTouch/Config.cs
+++ TouchChan.AssistiveTouch/Config.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using WindowsInput.Events;
+using Windows.Win32;
+using Windows.Win32.Foundation;
 
 namespace TouchChan.AssistiveTouch
 {
@@ -88,9 +90,9 @@ namespace TouchChan.AssistiveTouch
 
             public string? Read(string key)
             {
-                var RetVal = new StringBuilder(255);
-                Kernel32.GetPrivateProfileString(Section, key, string.Empty, RetVal, 255, IniPath);
-                return RetVal.ToString() == string.Empty ? null : RetVal.ToString();
+                var retVal = new PWSTR();
+                PInvoke.GetPrivateProfileString(Section, key, string.Empty, retVal, 255, IniPath);
+                return retVal.ToString() == string.Empty ? null : retVal.ToString();
             }
 
             public void Write(string key, string value) => Kernel32.WritePrivateProfileString(Section, key, value, IniPath);
