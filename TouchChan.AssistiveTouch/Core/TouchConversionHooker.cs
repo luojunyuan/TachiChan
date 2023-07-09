9ab80d0b06b7723e26c7cfcb6ac7e407ccbd1731
k1mlka <k1mlka@outlook.com>
2023-07-10 00:37:14 +0900

    .

--- TouchChan.AssistiveTouch/Core/TouchConversionHooker.cs
+++ TouchChan.AssistiveTouch/Core/TouchConversionHooker.cs
@@ -33,41 +33,42 @@ public static class TouchConversionHooker
         var info = Marshal.PtrToStructure<User32.MSLLHOOKSTRUCT>(lParam);
 
         var extraInfo = (uint)info.dwExtraInfo;
-        if ((extraInfo & MOUSEEVENTF_FROMTOUCH) == MOUSEEVENTF_FROMTOUCH)
+        if ((extraInfo & MOUSEEVENTF_FROMTOUCH) != MOUSEEVENTF_FROMTOUCH)
+            return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
+        
+        var isGameWindow = User32.GetForegroundWindow() == App.GameWindowHandle;
+        if (!isGameWindow)
+            return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
+
+        var win = (MainWindow)Application.Current.MainWindow;
+        var winOrigin = new System.Drawing.Point();
+        User32.MapWindowPoints(MainWindow.Handle, IntPtr.Zero, ref winOrigin);
+        var relativePoint = new Point((info.pt.X - winOrigin.X) / win.Dpi, (info.pt.Y - winOrigin.Y) / win.Dpi);
+        // Hit test if its touchchan itself
+        if (VisualTreeHelper.HitTest((Grid)win.Content, relativePoint) != null 
+            || relativePoint.Y < 0)
+            return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
+
+        switch ((int)wParam)
         {
-            var isGameWindow = User32.GetForegroundWindow() == App.GameWindowHandle;
-            if (!isGameWindow)
-                return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
-
-            var win = (MainWindow)Application.Current.MainWindow;
-            var winOrigin = new System.Drawing.Point();
-            User32.MapWindowPoints(MainWindow.Handle, IntPtr.Zero, ref winOrigin);
-            var relativePoint = new Point((info.pt.X - winOrigin.X) / win.Dpi, (info.pt.Y - winOrigin.Y) / win.Dpi);
-            if (VisualTreeHelper.HitTest((Grid)win.Content, relativePoint) != null ||
-                relativePoint.Y < 0)
-                return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
-
-            switch ((int)wParam)
-            {
-                case 0x202:
-                    Task.Run(() =>
-                    {
-                        var (x, y) = GetCursorPosition();
-                        User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_LEFTDOWN, x, y, 0, IntPtr.Zero);
-                        Thread.Sleep(UserTimerMinimum);
-                        User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_LEFTUP, x, y, 0, IntPtr.Zero);
-                    });
-                    break;
-                case 0x205:
-                    Task.Run(() =>
-                    {
-                        var (x, y) = GetCursorPosition();
-                        User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_RIGHTDOWN, x, y, 0, IntPtr.Zero);
-                        Thread.Sleep(UserTimerMinimum);
-                        User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_RIGHTUP, x, y, 0, IntPtr.Zero);
-                    });
-                    break;
-            }
+            case 0x202:
+                Task.Run(() =>
+                {
+                    var (x, y) = GetCursorPosition();
+                    User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_LEFTDOWN, x, y, 0, IntPtr.Zero);
+                    Thread.Sleep(UserTimerMinimum);
+                    User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_LEFTUP, x, y, 0, IntPtr.Zero);
+                });
+                break;
+            case 0x205:
+                Task.Run(() =>
+                {
+                    var (x, y) = GetCursorPosition();
+                    User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_RIGHTDOWN, x, y, 0, IntPtr.Zero);
+                    Thread.Sleep(UserTimerMinimum);
+                    User32.mouse_event(User32.MOUSEEVENTF.MOUSEEVENTF_RIGHTUP, x, y, 0, IntPtr.Zero);
+                });
+                break;
         }
 
         return User32.CallNextHookEx(_hookId!, nCode, wParam, lParam);
