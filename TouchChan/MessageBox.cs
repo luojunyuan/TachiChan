7bbd36c5d87e349bb0db7cbcafeb512a31fbef65
k1mlka <k1mlka@outlook.com>
2023-07-09 20:35:50 +0900

    .

--- TouchChan/MessageBox.cs
+++ TouchChan/MessageBox.cs
@@ -5,7 +5,7 @@ namespace TouchChan;
 
 internal class MessageBox
 {
-    public static void Show(string text, string title = "TouchChan", IntPtr parent = default) => _ = _MessageBox(parent, text, title, MB_TOPMOST | MB_SETFOREGROUND);
+    public static void Show(string text, string title = "TachiChan", IntPtr parent = default) => _ = _MessageBox(parent, text, title, MB_TOPMOST | MB_SETFOREGROUND);
 
     [DllImport("user32.dll", EntryPoint = "MessageBoxA", ExactSpelling = true, CharSet = CharSet.Ansi)]
     public static extern int _MessageBox(IntPtr hWnd, string lpText, string lpCaption, int uType);
--- TouchChan/Strings.cs
+++ TouchChan/Strings.cs
@@ -2,7 +2,7 @@
 
 internal class Strings
 {
-    public const string App_StartNoParameter = "Can't open TouchChan directly ";
+    public const string App_StartNoParameter = "Can't open TachiChan directly ";
     public const string App_StartInvalidPath = "Invalid game path ";
     public const string App_Timeout = "Timeout! Game window not found.";
     public const string App_LENotSetup = "Locate Emulator not found, please set LEProc path in Preference.";
