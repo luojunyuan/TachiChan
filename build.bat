3e2bc88eb4454b5a416e4e7948f34f12a01c7cfd
ami <ami@MINIBOOK>
2023-08-02 16:10:57 +0900

    delete files

--- build.bat
+++
@@ -1,7 +0,0 @@
-REM this file is for testing temporary using. Aim at net7 arm64
-set dest=../Release/TachiChan
-dotnet build TouchChan/TouchChan.csproj -c Release -r win-arm64 -o %dest%/TouchChan
-dotnet build TouchChan.AssistiveTouch/TouchChan.AssistiveTouch.csproj -c Release -r win-arm64 -o %dest%/TouchChan.AssistiveTouch
-dotnet build TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj -c Release -r win-arm64 -o %dest%/TouchChan.AssistiveTouch.Gesture --no-self-contained
-powershell -Command "& {ls ../Release/TachiChan -include *.pdb -recurse | rm}"
-powershell -Command "& {ls ../Release/TachiChan -include *.exe.config -recurse | rm}"
--- build472.bash
+++
@@ -1,3 +0,0 @@
-dotnet build -c Release -o Publish/TachiChan TouchChan/TouchChan.csproj
-dotnet build -c Release -o Publish/TachiChan TouchChan.AssistiveTouch/TouchChan.AssistiveTouch.csproj
-dotnet build -c Release -o Publish/TachiChan TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj
