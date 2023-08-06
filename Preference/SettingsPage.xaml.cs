9106d9a5d0d97edb8ab42b1c2976d02c4cba3558
ami <ami@MINIBOOK>
2023-08-06 09:24:59 +0900

    read write

--- Preference/SettingsPage.xaml.cs
+++ Preference/SettingsPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿#nullable enable
+using IniParser;
+using System;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
@@ -25,6 +16,58 @@ namespace Preference
         public SettingsPage()
         {
             this.InitializeComponent();
+
+            Loaded += async (_, _) =>
+            {
+                StorageFolder roamingFolder = ApplicationData.Current.RoamingFolder;
+                IStorageItem item = await roamingFolder.TryGetItemAsync("Config.ini");
+
+                if (item is not StorageFile iniFile)
+                    return;
+                
+                string fileContent = await FileIO.ReadTextAsync(iniFile);
+
+                var parser = new FileIniDataParser();
+                var data = parser.Parser.Parse(fileContent);
+
+                string value = data["TouchChan"]["ScreenShotTradition"] ?? "false";
+                string value2 = data["TouchChan"]["UseEnterKeyMapping"] ?? "false";
+                //string value = data["TouchChan"]["MappingKey"];
+                //string value = data["TouchChan"]["ModernSleep"]; 
+
+                Screenshot.IsChecked = bool.Parse(value);
+                EnterKeyMapping.IsChecked = bool.Parse(value2);
+            };
+        }
+
+        private async void Screenshot_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            StorageFolder roamingFolder = ApplicationData.Current.RoamingFolder;
+            var iniFile = await roamingFolder.CreateFileAsync("Config.ini", CreationCollisionOption.OpenIfExists);
+
+            string fileContent = await FileIO.ReadTextAsync(iniFile);
+
+            var parser = new FileIniDataParser();
+            var data = parser.Parser.Parse(fileContent);
+
+            data["TouchChan"]["ScreenShotTradition"] = Screenshot.IsChecked.ToString();
+            string newFileContent = data.ToString();
+            await FileIO.WriteTextAsync(iniFile, newFileContent);
+        }
+
+        private async void EnterKeyMapping_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            StorageFolder roamingFolder = ApplicationData.Current.RoamingFolder;
+            var iniFile = await roamingFolder.CreateFileAsync("Config.ini", CreationCollisionOption.OpenIfExists);
+
+            string fileContent = await FileIO.ReadTextAsync(iniFile);
+
+            var parser = new FileIniDataParser();
+            var data = parser.Parser.Parse(fileContent);
+
+            data["TouchChan"]["UseEnterKeyMapping"] = EnterKeyMapping.IsChecked.ToString();
+            string newFileContent = data.ToString();
+            await FileIO.WriteTextAsync(iniFile, newFileContent);
         }
     }
 }
