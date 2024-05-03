efb46a7f056a8eee9118928837dc0a71f5c3de30
k1mlka <k1mlka@outlook.com>
2024-05-03 17:39:20 +0800

    fix: Shell build

--- build472.sh
+++ build472.sh
@@ -5,11 +5,11 @@ xml_files=("TouchChan/TouchChan.csproj"
     "TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj"
 )
 for xml_file in "${xml_files[@]}"; do
-    sed -i 's|<TargetFramework>.*</TargetFramework>|<TargetFramework>net472</TargetFramework>|' "$xml_file"
+    sed -i '' -e 's|<TargetFramework>.*</TargetFramework>|<TargetFramework>net472</TargetFramework>|' "$xml_file"
 done
 dotnet build -c Release -o bin/TachiChan TouchChan/TouchChan.csproj
 dotnet build -c Release -o bin/TachiChan TouchChan.AssistiveTouch/TouchChan.AssistiveTouch.csproj
 dotnet build -c Release -o bin/TachiChan TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj
 dotnet build -c Release -o bin/TachiChan Preference.Win32/Preference.Win32.csproj
 find bin/TachiChan -type f \( -name "*.pdb" -o -name "*.exe.config" \) -exec rm -f {} \;
-tar -czf net472.tar.gz -C bin/TachiChan .
+tar -czf bin/net472.tar.gz -C bin/TachiChan .
