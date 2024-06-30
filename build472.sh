3a2fc6d13fe4f01b6297c355279c1ff47ec206b9
k1mlka <k1mlka@outlook.com>
2024-06-30 11:44:20 +0800

    build: Add shell build for gamepad

--- build472.sh
+++ build472.sh
@@ -3,6 +3,7 @@ xml_files=("TouchChan/TouchChan.csproj"
     "TouchChan.SplashScreen/TouchChan.SplashScreen.csproj"
     "TouchChan.AssistiveTouch/TouchChan.AssistiveTouch.csproj"
     "TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj"
+    "TouchChan.AssistiveTouch.Gamepad/TouchChan.AssistiveTouch.Gamepad.csproj"
 )
 for xml_file in "${xml_files[@]}"; do
     sed -i '' -e 's|<TargetFramework>.*</TargetFramework>|<TargetFramework>net472</TargetFramework>|' "$xml_file"
@@ -10,6 +11,7 @@ done
 dotnet build -c Release -o bin/TachiChan TouchChan/TouchChan.csproj
 dotnet build -c Release -o bin/TachiChan TouchChan.AssistiveTouch/TouchChan.AssistiveTouch.csproj
 dotnet build -c Release -o bin/TachiChan TouchChan.AssistiveTouch.Gesture/TouchChan.AssistiveTouch.Gesture.csproj
+dotnet build -c Release -o bin/TachiChan TouchChan.AssistiveTouch.Gamepad/TouchChan.AssistiveTouch.Gamepad.csproj
 dotnet build -c Release -o bin/TachiChan Preference.Win32/Preference.Win32.csproj
 find bin/TachiChan -type f \( -name "*.pdb" -o -name "*.exe.config" \) -exec rm -f {} \;
 tar -czf bin/net472.tar.gz -C bin/TachiChan .
